735096817461520cb9072597677904cb851a5a56
kailash <kailash@kashsolution.com>
2022-07-18 00:17:54 +0530

    PHP UNIT Test install

--- docker-install.sh
+++ docker-install.sh
@@ -4,4 +4,6 @@ composer install
 php bin/console assets:install --symlink --relative
 php bin/console doctrine:schema:update --force
 php bin/console lexik:jwt:generate-keypair --skip-if-exists
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:schema:update --force --env=test
 exec php-fpm
